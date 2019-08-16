081e3fb38213b4111f794288a53dde5580be854e
Parsa Sam <parsasam1378@gmail.com>
2019-08-16 18:14:23 +0430

    initial settings added

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-gcc main.c ./player/player.c -o main && sleep 2
+rm -r ./data/*
+
+echo "void player_turn()
+{
+    // your code goes here
+}" > "./data/player.c"
+
+gcc main.c ./player/player.c -o main
