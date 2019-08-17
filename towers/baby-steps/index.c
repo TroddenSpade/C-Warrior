b95c7e1eaba5f631cf0c8b360d5d69e5a9991415
Parsa Sam <parsasam1378@gmail.com>
2019-08-18 02:15:10 +0430

    walk function added

--- towers/baby-steps/index.c
+++ towers/baby-steps/index.c
@@ -1,38 +1,44 @@
 #define NUMBER_OF_LEVELS 2
+#define NUMBER_OF_UNITS 2
 
-struct Level
-{
-    int timeBonus;
-    int aceScore;
-    int width;
-    int height;
-    int stairX;
-    int stairY;
-    int maxHealth;
-    int startX;
-    int startY;
-} levels[NUMBER_OF_LEVELS];
+struct Level levels[NUMBER_OF_LEVELS];
+struct Unit units[NUMBER_OF_UNITS];
 
-void constructor()
+void levelConstructor()
 {
     levels[0].height = 1;
     levels[0].width = 8;
     levels[0].aceScore = 26;
     levels[0].timeBonus = 20;
-    levels[0].stairX = 7;
-    levels[0].stairY = 0;
+    levels[0].startX = 1;
+    levels[0].startY = 1;
     levels[0].maxHealth = 20;
-    levels[0].stairX = 0;
-    levels[0].stairY = 0;
+    levels[0].stairX = 8;
+    levels[0].stairY = 1;
+    levels[0].unitsLength = 1;
+    levels[0].thisLevelsUnits[0] = units[0];
+}
 
-    levels[1].height = 1;
-    levels[1].width = 1;
-    levels[1].aceScore = 1;
-    levels[1].timeBonus = 1;
-    levels[1].stairX = 1;
-    levels[1].stairY = 1;
-    levels[1].maxHealth = 1;
-    levels[1].stairX = 0;
-    levels[1].stairY = 0;
-    puts("set");
+void unitConstructor()
+{
+    units[0].character = '@';
+    units[0].health = 20;
+    units[0].locX = 1;
+    units[0].locY = 1;
+    units[0].enemy = 0;
+    strcpy(units[0].name, "Warrior");
+}
+
+void walk(struct Unit *unit)
+{
+    if (isEmpty())
+    {
+        move(unit);
+
+        cLog(unit, "is moving");
+    }
+    else
+    {
+        cLog(unit, "cant move");
+    }
 }
