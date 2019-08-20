8cb84fb0e1a1f3b4d1b8344918d85b6133508561
Parsa Sam <parsasam1378@gmail.com>
2019-08-20 15:20:48 +0430

    run scripts added

--- run.bat
+++ run.bat
@@ -1,3 +1,20 @@
 del ".\main.exe" /s /f /q
-gcc main.c ./player/player.c -o main
+
+if exist .\data\Player.c (
+    echo ".\data\Player.c  exists"
+) else (
+    echo ".\data\Player.c  doesn't exist, creating a new Player.c ..."
+    @echo off
+    (
+    echo void playTurn(^)
+    echo {
+    echo     // your code goes here
+    echo }
+    ) >.\data\Player.c 
+)
+
+gcc main.c .\player\player.c -o main
+
+REM cls
+
 .\main
--- run.sh
+++ run.sh
@@ -1,5 +1,20 @@
 rm -r ./main.exe
 rm -r ./main
 rm -r ./main.out
+
+FILE=./data/Player.c
+if [ -f "$FILE" ]; then
+    echo "$FILE exist"
+else 
+    echo "$FILE does not exist, creating a new player.c ..."
+    echo "void playTurn()
+{
+    // your code goes here
+}" > "$FILE"
+fi
+
 gcc main.c ./player/player.c -o main
+
+clear
+
 ./main
