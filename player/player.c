a4d72aebba58e32e7f8d8c8435125d908e7f7ecf
Parsa Sam <parsasam1378@gmail.com>
2019-08-20 15:22:09 +0430

    check and objectsRun functions added

--- player/player.c
+++ player/player.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include "player.h"
+#include "../main.h"
+#include "structs.c"
 
+#define SLEEP_TIME 2
 #define MAX_RUN_TIME 100
-#define LVL_HEADER "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ level ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+#define Warrior &warrior
+#define LVL_HEADER "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LEVEL ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 #define TURN_L "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~ "
 #define TURN_R " ~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-#define WARRIOR &units[0]
 #define WALL 'W'
 
+int isLevelOn = 1;
 int timeBonus;
 int aceScore;
-int stairX;
-int stairY;
-int health;
 int score = 0;
-char field[10][20];
+struct Object *field[10][20];
 
-#include "player.h"
-#include "structs.c"
-#include "space.c"
-#include "unit.c"
+#include "methods.c"
 #include "../towers/baby-steps/index.c"
-#include "../data/player.c"
+#include "../data/Player.c"
 
 #ifdef _WIN32
 #include <fcntl.h>
@@ -46,21 +45,28 @@ char field[10][20];
 #define VER "\u2550"
 #define HOR "\u2551"
 #define _WIN32 0
-#define _O_TEXT 0
 #endif
 
 void setupField(int level);
+void setupObjects();
 void draw(int level);
 void show(int level);
-int run(int level)
+void check(int level);
+void objectsRun(int level);
+
+int play(int level)
 {
-    unitConstructor();
+    objConstructor();
     levelConstructor();
-    health = levels[level].maxHealth;
-    int counter = 0;
+    setupObjects();
     setupField(level);
     puts(LVL_HEADER);
-    while (counter < MAX_RUN_TIME)
+
+    printf("%s000%s\n", TURN_L, TURN_R);
+    draw(level);
+    sleep(SLEEP_TIME);
+    int counter = 1;
+    while (isLevelOn)
     {
         char turn[80];
         char step[3];
@@ -70,11 +76,11 @@ int run(int level)
         strcat(turn, step);
         strcat(turn, TURN_R);
         puts(turn);
-
+        playTurn();
+        objectsRun(level);
         draw(level);
-        player_turn();
-
-        sleep(2);
+        check(level);
+        sleep(SLEEP_TIME);
     }
 }
 
@@ -82,7 +88,7 @@ void draw(int level)
 {
     if (__APPLE__)
     {
-        printf("%s %d\n", HEART, health);
+        printf("%s %d\n", HEART, warrior.health);
         printf("%s %d\n", SCORE, score);
         printf("%s", UP_LEFT);
         for (int j = 0; j < levels[level].width; j++)
@@ -95,7 +101,7 @@ void draw(int level)
             printf("%s", HOR);
             for (int j = 0; j < levels[level].width; j++)
             {
-                printf("%c", field[i + 1][j + 1]);
+                printf("%c", field[i + 1][j + 1]->character);
             }
             printf("%s\n", HOR);
         }
@@ -110,56 +116,56 @@ void draw(int level)
     {
         _setmode(_fileno(stdout), 0x00020000);
         wprintf(HEART);
-        _setmode(_fileno(stdout), _O_TEXT);
-        printf(" %d\n", health);
+        _setmode(_fileno(stdout), 0x4000);
+        printf(" %d\n", warrior.health);
 
         _setmode(_fileno(stdout), 0x00020000);
         wprintf(SCORE);
-        _setmode(_fileno(stdout), _O_TEXT);
+        _setmode(_fileno(stdout), 0x4000);
         printf(" %d\n", score);
 
         _setmode(_fileno(stdout), 0x00020000);
         wprintf(UP_LEFT);
-        _setmode(_fileno(stdout), _O_TEXT);
+        _setmode(_fileno(stdout), 0x4000);
         for (int j = 0; j < levels[level].width; j++)
         {
             _setmode(_fileno(stdout), 0x00020000);
             wprintf(VER);
-            _setmode(_fileno(stdout), _O_TEXT);
+            _setmode(_fileno(stdout), 0x4000);
         }
 
         _setmode(_fileno(stdout), 0x00020000);
         wprintf(UP_RIGHT);
-        _setmode(_fileno(stdout), _O_TEXT);
+        _setmode(_fileno(stdout), 0x4000);
         puts("");
 
         for (int i = 0; i < levels[level].height; i++)
         {
             _setmode(_fileno(stdout), 0x00020000);
             wprintf(HOR);
-            _setmode(_fileno(stdout), _O_TEXT);
+            _setmode(_fileno(stdout), 0x4000);
             for (int j = 0; j < levels[level].width; j++)
             {
-                printf("%c", field[i + 1][j + 1]);
+                printf("%c", field[i + 1][j + 1]->character);
             }
             _setmode(_fileno(stdout), 0x00020000);
             wprintf(HOR);
-            _setmode(_fileno(stdout), _O_TEXT);
+            _setmode(_fileno(stdout), 0x4000);
             puts("");
         }
 
         _setmode(_fileno(stdout), 0x00020000);
         wprintf(DOWN_LEFT);
-        _setmode(_fileno(stdout), _O_TEXT);
+        _setmode(_fileno(stdout), 0x4000);
         for (int j = 0; j < levels[level].width; j++)
         {
             _setmode(_fileno(stdout), 0x00020000);
             wprintf(VER);
-            _setmode(_fileno(stdout), _O_TEXT);
+            _setmode(_fileno(stdout), 0x4000);
         }
         _setmode(_fileno(stdout), 0x00020000);
         wprintf(DOWN_RIGHT);
-        _setmode(_fileno(stdout), _O_TEXT);
+        _setmode(_fileno(stdout), 0x4000);
         puts("");
     }
 }
@@ -169,28 +175,35 @@ void setupField(int level)
 
     for (int i = 0; i <= levels[level].width + 1; i++)
     {
-        field[0][i] = WALL;
+        field[0][i] = &wall;
     }
 
     for (int i = 1; i <= levels[level].height; i++)
     {
-        field[i][0] = WALL;
+        field[i][0] = &wall;
+
         for (int j = 1; j <= levels[level].width; j++)
         {
-            field[i][j] = ' ';
+            field[i][j] = &space;
         }
-        field[i][levels[level].width + 1] = WALL;
+        field[i][levels[level].width + 1] = &wall;
     }
+
+    puts("yellow");
 
     for (int i = 0; i <= levels[level].width + 1; i++)
     {
-        field[levels[level].height + 1][i] = WALL;
+        field[levels[level].height + 1][i] = &wall;
     }
 
-    for (int i = 0; i < levels[level].unitsLength; i++)
+    field[levels[level].stairY][levels[level].stairX] = &stairs;
+
+    for (int i = 0; i < levels[level].objsLength; i++)
     {
-        field[levels[level].thisLevelsUnits[i].locY][levels[level].thisLevelsUnits[i].locX] = levels[level].thisLevelsUnits[i].character;
+        field[levels[level].thisLevelsObjs[i]->locY][levels[level].thisLevelsObjs[i]->locX] = levels[level].thisLevelsObjs[i];
     }
+
+    field[warrior.locY][warrior.locX] = &warrior;
 }
 
 void show(int level)
@@ -199,8 +212,54 @@ void show(int level)
     {
         for (int i = 0; i <= levels[level].width + 1; i++)
         {
-            printf("%c", field[j][i]);
+            printf("%s ,", field[j][i]->name);
         }
         puts("");
+    }
+}
+
+void check(int level)
+{
+    if (warrior.locX == levels[level].stairX && warrior.locY == levels[level].stairY)
+    {
+        isLevelOn = 0;
+        printf("%s", "LEVEL Done!");
+        generateLevel(level + 2, 0);
+    }
+    else if (warrior.health == 0)
+    {
+        isLevelOn = 0;
+        printf("Warrior Died!, you failed level %d", level + 1);
+    }
+}
+
+void setupObjects()
+{
+    warrior.character = '@';
+    warrior.health = 20;
+    warrior.locX = 1;
+    warrior.locY = 1;
+    warrior.enemy = 0;
+    warrior.face = EAST;
+    strcpy(warrior.name, "Warrior");
+
+    wall.character = 'W';
+    wall.enemy = 0;
+    strcpy(wall.name, "Wall");
+
+    space.character = ' ';
+    space.enemy = 0;
+    strcpy(space.name, "Space");
+
+    stairs.character = '>';
+    stairs.enemy = 0;
+    strcpy(stairs.name, "Stairs");
+}
+
+void objectsRun(int level)
+{
+    for (int i = 0; i < levels[level].objsLength; i++)
+    {
+        levels[level].thisLevelsObjs[0]->run(levels[level].thisLevelsObjs[0]);
     }
 }
